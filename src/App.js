6708eb0c1c5bbd8c621647d84b91aa3fd9fd1636
pawel_admin <ptcebula@gmail.com>
2020-06-28 17:50:14 +0200

    Fixed index.css import in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Restaurant from './Restaurant';
+import './index.css';
 // import City from './City';
 
 function App() {
