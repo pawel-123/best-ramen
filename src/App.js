04f4eb36a68ff70eeefca478a94a0e88d0662bc6
pawel_admin <ptcebula@gmail.com>
2020-10-05 19:20:47 +0200

    feat: Implement isLoading and isError state

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ function App() {
 
     const [restaurants, setRestaurants] = useState([]);
     const [count, setCount] = useState(10);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
+
         const getRestaurants = async () => {
-            const data = await fetchData(count);
-            setRestaurants(data.restaurants);
+            try {
+                const data = await fetchData(count);
+                setRestaurants(data.restaurants);
+            } catch (error) {
+                setIsError(true);
+            }
+            setIsLoading(false);
         }
+
         getRestaurants();
     }, [count]);
 
@@ -26,15 +36,23 @@ function App() {
         <div>
             <Header />
             <input type="number" onChange={onChange}></input>
-            {restaurants.map(restaurant => (
-                <Restaurant
-                    key={restaurant.restaurant.id}
-                    name={restaurant.restaurant.name}
-                    city={restaurant.restaurant.location.city}
-                    rating={restaurant.restaurant.user_rating.aggregate_rating}
-                    photo={restaurant.restaurant.featured_image ? restaurant.restaurant.featured_image : "https://static.thenounproject.com/png/18272-200.png"}
-                />
-            ))}
+
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                    restaurants.map(restaurant => (
+                        <Restaurant
+                            key={restaurant.restaurant.id}
+                            name={restaurant.restaurant.name}
+                            city={restaurant.restaurant.location.city}
+                            rating={restaurant.restaurant.user_rating.aggregate_rating}
+                            photo={restaurant.restaurant.featured_image ? restaurant.restaurant.featured_image : "https://static.thenounproject.com/png/18272-200.png"}
+                        />
+                    ))
+                )}
+
+            {isError && <p>Error in fetching restaurants</p>}
+
         </div>
     )
 }
