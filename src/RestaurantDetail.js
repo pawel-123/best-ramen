4cc4f8212520906eac0bcb8e5407b04ba41b1984
pawel_admin <ptcebula@gmail.com>
2020-10-06 17:20:58 +0200

    refactor: Move fetchRestaurant to api.js

--- src/RestaurantDetail.js
+++ src/RestaurantDetail.js
@@ -1,29 +1,22 @@
 import React, { useEffect, useState } from 'react';
-// import { fetchRestaurant } from './api/api'
-import { zomatoApiBase } from './constants/constants'
+import { fetchRestaurant } from './api/api'
 
 const RestaurantDetail = ({ match }) => {
 
     const [restaurant, setRestaurant] = useState({});
 
     useEffect(() => {
-        fetchRestaurant();
+        const getRestaurantDetail = async () => {
+            const data = await fetchRestaurant();
+            setRestaurant(data);
+        }
+
+        getRestaurantDetail();
         // added eslint-disable-next-line based on warning when compiling:
         // "React Hook useEffect has a missing dependency: 'fetchRestaurant'. Either include it or remove the dependency array"
         // is there a better way to solve this?
         // eslint-disable-next-line
     }, [restaurant]);
-
-    const fetchRestaurant = async () => {
-        const response = await fetch(`${zomatoApiBase}restaurant?res_id=${match.params.id}`, {
-            headers: {
-                Accept: "application/json",
-                "User-Key": "12bc09e5b12689345f110bb8f2d76b3c"
-            }
-        });
-        const data = await response.json();
-        setRestaurant(data);
-    }
 
     return (
         <div>
--- src/api/api.js
+++ src/api/api.js
@@ -11,13 +11,13 @@ export const fetchData = async (count) => {
     return data;
 }
 
-// export const fetchRestaurant = async () => {
-//     const response = await fetch(`${zomatoApiBase}restaurant?res_id={16774318}`, {
-//         headers: {
-//             Accept: "application/json",
-//             "User-Key": "d1b1d251eb3a0f96b582683ef476f0b0"
-//         }
-//     });
-//     const data = await response.json();
-//     return data;
-// }
+export const fetchRestaurant = async ({ match }) => {
+    const response = await fetch(`${zomatoApiBase}restaurant?res_id=${match.params.id}`, {
+        headers: {
+            Accept: "application/json",
+            "User-Key": "12bc09e5b12689345f110bb8f2d76b3c"
+        }
+    });
+    const data = await response.json();
+    return data;
+}
